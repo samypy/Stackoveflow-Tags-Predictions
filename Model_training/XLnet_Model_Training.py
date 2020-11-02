7c330c105f7ba13f183a965b6a43a2dc23683804
samypy <59316197+samypy@users.noreply.github.com>
2020-11-02 15:54:50 +0530

    Add files via upload

---
+++ Model_training/XLnet_Model_Training.py
@@ -0,0 +1,496 @@
+
+#pip install transformers
+
+# Commented out IPython magic to ensure Python compatibility.
+import os
+import math
+import torch
+from torch.nn import BCEWithLogitsLoss
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import AdamW, XLNetTokenizer, XLNetModel, XLNetLMHeadModel, XLNetConfig
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+from tqdm import tqdm, trange
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+print("GPU Available: {}".format(torch.cuda.is_available()))
+n_gpu = torch.cuda.device_count()
+print("Number of GPU Available: {}".format(n_gpu))
+print("GPU: {}".format(torch.cuda.get_device_name(0)))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+device
+
+"""## Import Dataset"""
+
+# get small sample of train to try
+#train = train.sample(n=100)
+
+import pickle
+
+flattened = pd.read_pickle("flattened.pkl")
+flattened.shape
+
+flattened.head(2)
+
+train,test = train_test_split(flattened,test_size=0.3)
+
+train.shape,test.shape
+
+"""## Preprocess Data"""
+
+tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=True)
+
+train_text_list = train["Title & Body"].values
+test_text_list = test["Title & Body"].values
+
+def tokenize_inputs(text_list, tokenizer, num_embeddings=512):
+    """
+    Tokenizes the input text input into ids. Appends the appropriate special
+    characters to the end of the text to denote end of sentence. Truncate or pad
+    the appropriate sequence length.
+    """
+    # tokenize the text, then truncate sequence to the desired length minus 2 for
+    # the 2 special characters
+    tokenized_texts = list(map(lambda t: tokenizer.tokenize(t)[:num_embeddings-2], text_list))
+    # convert tokenized text into numeric ids for the appropriate LM
+    input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+    # append special token "<s>" and </s> to end of sentence
+    input_ids = [tokenizer.build_inputs_with_special_tokens(x) for x in input_ids]
+    # pad sequences
+    input_ids = pad_sequences(input_ids, maxlen=num_embeddings, dtype="long", truncating="post", padding="post")
+    return input_ids
+
+def create_attn_masks(input_ids):
+    """
+    Create attention masks to tell model whether attention should be applied to
+    the input id tokens. Do not want to perform attention on padding tokens.
+    """
+    # Create attention masks
+    attention_masks = []
+
+    # Create a mask of 1s for each token followed by 0s for padding
+    for seq in input_ids:
+        seq_mask = [float(i>0) for i in seq]
+        attention_masks.append(seq_mask)
+    return attention_masks
+
+# create input id tokens
+train_input_ids = tokenize_inputs(train_text_list, tokenizer, num_embeddings=250)
+train_input_ids
+
+# create input id tokens
+test_input_ids = tokenize_inputs(test_text_list, tokenizer, num_embeddings=250)
+test_input_ids
+
+# create attention masks
+train_attention_masks = create_attn_masks(train_input_ids)
+#train_attention_masks
+
+# create attention masks
+test_attention_masks = create_attn_masks(test_input_ids)
+#test_attention_masks
+
+# add input ids and attention masks to the dataframe
+train["features"] = train_input_ids.tolist()
+train["masks"] = train_attention_masks
+
+test["features"] = test_input_ids.tolist()
+test["masks"] = test_attention_masks
+
+train.head()
+
+test.head()
+
+"""## Train, Valid Split"""
+
+# train valid split
+train, valid = train_test_split(train, test_size=0.2, random_state=42)
+
+X_train = train["features"].values.tolist()
+X_valid = valid["features"].values.tolist()
+
+train_masks = train["masks"].values.tolist()
+valid_masks = valid["masks"].values.tolist()
+
+label_cols = ['android', 'c#', 'c++', 'html', 'ios', 'java','javascript', 'jquery', 'php', 'python']
+Y_train = train[label_cols].values.tolist()
+Y_valid = valid[label_cols].values.tolist()
+
+"""## Create Dataloaders"""
+
+# Convert all of our input ids and attention masks into 
+# torch tensors, the required datatype for our model
+
+X_train = torch.tensor(X_train)
+X_valid = torch.tensor(X_valid)
+
+Y_train = torch.tensor(Y_train, dtype=torch.float32)
+Y_valid = torch.tensor(Y_valid, dtype=torch.float32)
+
+train_masks = torch.tensor(train_masks, dtype=torch.long)
+valid_masks = torch.tensor(valid_masks, dtype=torch.long)
+
+# Select a batch size for training
+batch_size = 16
+
+# Create an iterator of our data with torch DataLoader. This helps save on 
+# memory during training because, unlike a for loop, 
+# with an iterator the entire dataset does not need to be loaded into memory
+
+train_data = TensorDataset(X_train, train_masks, Y_train)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data,\
+                              sampler=train_sampler,\
+                              batch_size=batch_size)
+
+validation_data = TensorDataset(X_valid, valid_masks, Y_valid)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data,\
+                                   sampler=validation_sampler,\
+                                   batch_size=batch_size)
+
+def train(model, num_epochs,\
+          optimizer,\
+          train_dataloader, valid_dataloader,\
+          model_save_path,\
+          train_loss_set=[], valid_loss_set = [],\
+          lowest_eval_loss=None, start_epoch=0,\
+          device="cpu"
+          ):
+  """
+  Train the model and save the model with the lowest validation loss
+  """
+
+  model.to(device)
+
+  # trange is a tqdm wrapper around the normal python range
+  for i in trange(num_epochs, desc="Epoch"):
+    # if continue training from saved model
+    actual_epoch = start_epoch + i
+
+    # Training
+
+    # Set our model to training mode (as opposed to evaluation mode)
+    model.train()
+
+    # Tracking variables
+    tr_loss = 0
+    num_train_samples = 0
+
+    # Train the data for one epoch
+    for step, batch in enumerate(train_dataloader):
+      # Add batch to GPU
+      batch = tuple(t.to(device) for t in batch)
+      # Unpack the inputs from our dataloader
+      b_input_ids, b_input_mask, b_labels = batch
+      # Clear out the gradients (by default they accumulate)
+      optimizer.zero_grad()
+      # Forward pass
+      loss = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
+      # store train loss
+      tr_loss += loss.item()
+      num_train_samples += b_labels.size(0)
+      # Backward pass
+      loss.backward()
+      # Update parameters and take a step using the computed gradient
+      optimizer.step()
+      #scheduler.step()
+
+    # Update tracking variables
+    epoch_train_loss = tr_loss/num_train_samples
+    train_loss_set.append(epoch_train_loss)
+
+    print("Train loss: {}".format(epoch_train_loss))
+
+    # Validation
+
+    # Put model in evaluation mode to evaluate loss on the validation set
+    model.eval()
+
+    # Tracking variables 
+    eval_loss = 0
+    num_eval_samples = 0
+
+    # Evaluate data for one epoch
+    for batch in valid_dataloader:
+      # Add batch to GPU
+      batch = tuple(t.to(device) for t in batch)
+      # Unpack the inputs from our dataloader
+      b_input_ids, b_input_mask, b_labels = batch
+      # Telling the model not to compute or store gradients,
+      # saving memory and speeding up validation
+      with torch.no_grad():
+        # Forward pass, calculate validation loss
+        loss = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
+        # store valid loss
+        eval_loss += loss.item()
+        num_eval_samples += b_labels.size(0)
+
+    epoch_eval_loss = eval_loss/num_eval_samples
+    valid_loss_set.append(epoch_eval_loss)
+
+    print("Valid loss: {}".format(epoch_eval_loss))
+
+    if lowest_eval_loss == None:
+      lowest_eval_loss = epoch_eval_loss
+      # save model
+      save_model(model, model_save_path, actual_epoch,\
+                 lowest_eval_loss, train_loss_set, valid_loss_set)
+    else:
+      if epoch_eval_loss < lowest_eval_loss:
+        lowest_eval_loss = epoch_eval_loss
+        # save model
+        save_model(model, model_save_path, actual_epoch,\
+                   lowest_eval_loss, train_loss_set, valid_loss_set)
+    print("\n")
+
+  return model, train_loss_set, valid_loss_set
+
+
+def save_model(model, save_path, epochs, lowest_eval_loss, train_loss_hist, valid_loss_hist):
+  """
+  Save the model to the path directory provided
+  """
+  model_to_save = model.module if hasattr(model, 'module') else model
+  checkpoint = {'epochs': epochs, \
+                'lowest_eval_loss': lowest_eval_loss,\
+                'state_dict': model_to_save.state_dict(),\
+                'train_loss_hist': train_loss_hist,\
+                'valid_loss_hist': valid_loss_hist
+               }
+  torch.save(checkpoint, save_path)
+  print("Saving model at epoch {} with validation loss of {}".format(epochs,\
+                                                                     lowest_eval_loss))
+  return
+  
+def load_model(save_path):
+  """
+  Load the model from the path directory provided
+  """
+  checkpoint = torch.load(save_path)
+  model_state_dict = checkpoint['state_dict']
+  model = XLNetForMultiLabelSequenceClassification(num_labels=model_state_dict["classifier.weight"].size()[0])
+  model.load_state_dict(model_state_dict)
+
+  epochs = checkpoint["epochs"]
+  lowest_eval_loss = checkpoint["lowest_eval_loss"]
+  train_loss_hist = checkpoint["train_loss_hist"]
+  valid_loss_hist = checkpoint["valid_loss_hist"]
+  
+  return model, epochs, lowest_eval_loss, train_loss_hist, valid_loss_hist
+
+"""## Train Model from Scratch"""
+
+torch.cuda.empty_cache()
+
+#config = XLNetConfig()
+        
+class XLNetForMultiLabelSequenceClassification(torch.nn.Module):
+  
+  def __init__(self, num_labels=2):
+    super(XLNetForMultiLabelSequenceClassification, self).__init__()
+    self.num_labels = num_labels
+    self.xlnet = XLNetModel.from_pretrained('xlnet-base-cased')
+    self.classifier = torch.nn.Linear(768, num_labels)
+
+    torch.nn.init.xavier_normal_(self.classifier.weight)
+
+  def forward(self, input_ids, token_type_ids=None,\
+              attention_mask=None, labels=None):
+    # last hidden layer
+    last_hidden_state = self.xlnet(input_ids=input_ids,\
+                                   attention_mask=attention_mask,\
+                                   token_type_ids=token_type_ids)
+    # pool the outputs into a mean vector
+    mean_last_hidden_state = self.pool_hidden_state(last_hidden_state)
+    logits = self.classifier(mean_last_hidden_state)
+        
+    if labels is not None:
+      loss_fct = BCEWithLogitsLoss()
+      loss = loss_fct(logits.view(-1, self.num_labels),\
+                      labels.view(-1, self.num_labels))
+      return loss
+    else:
+      return logits
+    
+  def freeze_xlnet_decoder(self):
+    """
+    Freeze XLNet weight parameters. They will not be updated during training.
+    """
+    for param in self.xlnet.parameters():
+      param.requires_grad = False
+    
+  def unfreeze_xlnet_decoder(self):
+    """
+    Unfreeze XLNet weight parameters. They will be updated during training.
+    """
+    for param in self.xlnet.parameters():
+      param.requires_grad = True
+    
+  def pool_hidden_state(self, last_hidden_state):
+    """
+    Pool the output vectors into a single mean vector 
+    """
+    last_hidden_state = last_hidden_state[0]
+    mean_last_hidden_state = torch.mean(last_hidden_state, 1)
+    return mean_last_hidden_state
+    
+model = XLNetForMultiLabelSequenceClassification(num_labels=len(Y_train[0]))
+#model = torch.nn.DataParallel(model)
+#model.cuda()
+
+optimizer = AdamW(model.parameters(), lr=2e-5, weight_decay=0.01, correct_bias=False)
+#scheduler = WarmupLinearSchedule(optimizer, warmup_steps=num_warmup_steps, t_total=num_total_steps)  # PyTorch scheduler
+
+num_epochs=3
+
+cwd = os.getcwd()
+model_save_path = output_model_file = os.path.join(cwd, "drive/My Drive/Models/xlnet_toxic.bin")
+model, train_loss_set, valid_loss_set = train(model=model,\
+                                              num_epochs=num_epochs,\
+                                              optimizer=optimizer,\
+                                              train_dataloader=train_dataloader,\
+                                              valid_dataloader=validation_dataloader,\
+                                              model_save_path=model_save_path,\
+                                              device="cuda")
+
+# Plot loss
+num_epochs = np.arange(len(train_loss_set))
+
+fig, ax = plt.subplots(figsize=(10, 5));
+ax.plot(num_epochs, np.array(train_loss_set), label="Train Loss")
+ax.plot(num_epochs, np.array(valid_loss_set), 'g-', label="Valid Loss")
+#ax1.plot(episode_record, lose_record, 'r-', label="Lose %")
+ax.set_xlabel("Number of Epochs")
+ax.set_ylabel("Loss")
+ax.set_title("Loss vs Number of Epochs")
+
+"""## Train Model From Previous Checkpoint"""
+
+cwd = os.getcwd()
+model_save_path = output_model_file = os.path.join(cwd, "drive/My Drive/Models/xlnet_toxic.bin")
+model, start_epoch, lowest_eval_loss, train_loss_hist, valid_loss_hist = load_model(model_save_path)
+
+optimizer = AdamW(model.parameters(), lr=2e-5, weight_decay=0.01, correct_bias=False)
+
+num_epochs=1
+model, train_loss_set, valid_loss_set = train(model=model,\
+                                              num_epochs=num_epochs,\
+                                              optimizer=optimizer,\
+                                              train_dataloader=train_dataloader,\
+                                              valid_dataloader=validation_dataloader,\
+                                              model_save_path=model_save_path,\
+                                              train_loss_set=train_loss_hist,\
+                                              valid_loss_set=valid_loss_hist,\
+                                              lowest_eval_loss=lowest_eval_loss,\
+                                              start_epoch=start_epoch,\
+                                              device="cuda")
+
+torch.save(model,'model_stack.pt')
+
+# Plot loss
+num_epochs = np.arange(len(train_loss_set))
+
+fig, ax = plt.subplots(figsize=(10, 5));
+ax.plot(num_epochs, np.array(train_loss_set), label="Train Loss")
+ax.plot(num_epochs, np.array(valid_loss_set), 'g-', label="Valid Loss")
+#ax1.plot(episode_record, lose_record, 'r-', label="Lose %")
+ax.set_xlabel("Number of Epochs")
+ax.set_ylabel("Loss")
+ax.set_title("Loss vs Number of Epochs")
+
+"""## Get Predictions"""
+
+def generate_predictions(model, df, num_labels, device="cpu", batch_size=32):
+  num_iter = math.ceil(df.shape[0]/batch_size)
+  
+  pred_probs = np.array([]).reshape(0, num_labels)
+  
+  model.to(device)
+  model.eval()
+  
+  for i in range(num_iter):
+    df_subset = df.iloc[i*batch_size:(i+1)*batch_size,:]
+    X = df_subset["features"].values.tolist()
+    masks = df_subset["masks"].values.tolist()
+    X = torch.tensor(X)
+    masks = torch.tensor(masks, dtype=torch.long)
+    X = X.to(device)
+    masks = masks.to(device)
+    with torch.no_grad():
+      logits = model(input_ids=X, attention_mask=masks)
+      logits = logits.sigmoid().detach().cpu().numpy()
+      pred_probs = np.vstack([pred_probs, logits])
+  
+  return pred_probs
+
+num_labels = len(label_cols)
+pred_probs = generate_predictions(model, test, num_labels, device="cuda", batch_size=32)
+pred_probs
+
+label_cols = ['android', 'c#', 'c++', 'html', 'ios', 'java','javascript', 'jquery', 'php', 'python']
+
+test["android"] = pred_probs[:,0]
+test["c#"] = pred_probs[:,1]
+test["c++"] = pred_probs[:,2]
+test["html"] = pred_probs[:,3]
+test["ios"] = pred_probs[:,4]
+test["java"] = pred_probs[:,5]
+test["javascript"] = pred_probs[:,6]
+test["jquery"] = pred_probs[:,7]
+test["php"] = pred_probs[:,8]
+test["python"] = pred_probs[:,9]
+
+test.head()
+
+test_to_csv = test.reset_index()
+test_to_csv.head()
+
+cwd = os.getcwd()
+pred_save_path = output_model_file = os.path.join(cwd, "drive/My Drive/Data/toxic_1_epoch.csv")
+test_to_csv[["id", "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].to_csv(pred_save_path, index=False)
+
+pred = np.round(pred_probs)
+
+
+
+y_true = np.array([[0,0,1], [1,1,0],[0,1,0]])
+y_pred = pred
+
+labels = ["A", "B", "C"]
+
+conf_mat_dict={}
+
+for label_col in range(len(labels)):
+    y_true_label = y_true[:, label_col]
+    y_pred_label = y_pred[:, label_col]
+    conf_mat_dict[labels[label_col]] = confusion_matrix(y_pred=y_pred_label, y_true=y_true_label)
+
+
+for label, matrix in conf_mat_dict.items():
+    print("Confusion matrix for label {}:".format(label))
+    print(matrix)
+
+np.round(test[['android', 'c#', 'c++', 'html', 'ios', 'java','javascript', 'jquery', 'php', 'python']].head())
+
+flattened1 = pd.read_pickle("/content/drive/My Drive/Stack_Overflow/flattened.pkl")
+
+test_true = flattened1[250000:275000]
+del flattened1
+
+test_set  = test_true.copy()
+
+test_true.head()
+
+test_set_text_list = test_set["Title & Body"].values
+
+test_input_ids = tokenize_inputs(test_set_text_list, tokenizer, num_embeddings=250)
+test_input_ids
+
